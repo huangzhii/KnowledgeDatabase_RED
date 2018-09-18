1e4e5a93c5729abd1da897c95b26304fc28d84a6
Travis Johnson <johnstrs@i4.red.uits.iu.edu>
2018-09-18 16:11:40 -0400

    update

--- shiny/configuration.sh
+++ shiny/configuration.sh
@@ -1,3 +1,6 @@
 module load rstudio
 mkdir ~/r-packages
-R -e "install.packages(c('data.table', 'dplyr', 'tidyr', 'digest', 'DT', 'jsonlite', 'shinyWidgets'), lib='~/r-packages', repos='https://ftp.ussg.iu.edu/CRAN/')"
+R -e "install.packages(c('data.table', 'dplyr', 'tidyr', 'digest', 'DT', 'jsonlite', 'withr'), lib='~/r-packages', repos='https://ftp.ussg.iu.edu/CRAN/')"
+
+#R -e "library(withr)"
+#R -e "with_libpaths('~/r-packages', devtools::install_github('dreamRs/shinyWidgets'))"
